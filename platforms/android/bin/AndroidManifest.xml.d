7ed48450a2ec03347748041e3a998ba911c083da
Carsten Anton <carstenanton@gmail.com>
2013-12-14 18:17:41 +0100

    test KA

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/bin/AndroidManifest.xml : \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/gen/org/systax/SysTaxApp/R.java \
+ : /Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/drawable/icon.png \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/drawable-hdpi/icon.png \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/drawable-ldpi/icon.png \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/drawable-mdpi/icon.png \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/values/strings.xml \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/res/xml/config.xml \
+/Users/carstenanton/Documents/workspace/SysTaxApp/platforms/android/bin/AndroidManifest.xml \
